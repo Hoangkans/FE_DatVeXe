4eedf563e59b37e8dea24949571099b790332b3d
Hoangkans <thienkt179@gmail.com>
2026-03-14 14:44:15 +0700

    fix: increase axios timeout to 60000ms for render cold start

--- src/config/axios/axiosCongif.js
+++ src/config/axios/axiosCongif.js
@@ -1,109 +1,117 @@
 import axios from "axios";
 import BASE_API_URL from "../../services/base.api.url";
 
-import store from "../redux/store"
+import store from "../redux/store";
 import { logOut } from "../redux/reducers/user/userSlice";
 
 const api = axios.create({
-    baseURL: BASE_API_URL,
-    timeout: 5000,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+  baseURL: BASE_API_URL,
+  timeout: 60000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
 let isRefreshing = false;
 let failedQueue = [];
 
 const processQueue = (error, token = null) => {
-    failedQueue.forEach(prom => {
-        if (error) {
-            prom.reject(error);
-        } else {
-            prom.resolve(token);
-        }
-    });
-    failedQueue = [];
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
 };
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (err) => Promise.reject(err)
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err),
 );
 
 api.interceptors.response.use(
-    (response) => response,
-    async (err) => {
-        const originalRequest = err.config;
+  (response) => response,
+  async (err) => {
+    const originalRequest = err.config;
 
-        if (err.response && (err.response.status === 401 || err.response.status === 403) && originalRequest && !originalRequest._retry) {
-            
-            if (isRefreshing) {
-                return new Promise(function(resolve, reject) {
-                    failedQueue.push({ resolve, reject });
-                }).then(token => {
-                    originalRequest.headers['Authorization'] = 'Bearer ' + token;
-                    return api(originalRequest);
-                }).catch(err => {
-                    return Promise.reject(err);
-                });
-            }
+    if (
+      err.response &&
+      (err.response.status === 401 || err.response.status === 403) &&
+      originalRequest &&
+      !originalRequest._retry
+    ) {
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers["Authorization"] = "Bearer " + token;
+            return api(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
 
-            originalRequest._retry = true;
-            isRefreshing = true;
+      originalRequest._retry = true;
+      isRefreshing = true;
 
-            try {
-                const refreshToken = localStorage.getItem('refreshToken');
-                
-                if (!refreshToken) {
-                    throw new Error("No refresh token available");
-                }
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
 
-                const response = await axios.post(`${BASE_API_URL}/auth/refresh`, {
-                    refreshToken: refreshToken
-                });
-
-                const newToken = response.data.token || response.data.access_token;
-                const newRefreshToken = response.data.refresh_token;
-
-                if (!newToken) {
-                    throw new Error("Refresh failed: No new token returned");
-                }
-
-                localStorage.setItem('token', newToken);
-                if (newRefreshToken) {
-                    localStorage.setItem('refreshToken', newRefreshToken);
-                }
-                
-                api.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
-                originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
-
-                processQueue(null, newToken);
-                isRefreshing = false;
-
-                return api(originalRequest);
-
-            } catch (refreshError) {
-                processQueue(refreshError, null);
-                isRefreshing = false;
-                
-                console.error("Session expired completely. Logging out...", refreshError);
-                
-                store.dispatch(logOut());
-                
-                return Promise.reject(refreshError);
-            }
+        if (!refreshToken) {
+          throw new Error("No refresh token available");
         }
 
-        return Promise.reject(err);
+        const response = await axios.post(`${BASE_API_URL}/auth/refresh`, {
+          refreshToken: refreshToken,
+        });
+
+        const newToken = response.data.token || response.data.access_token;
+        const newRefreshToken = response.data.refresh_token;
+
+        if (!newToken) {
+          throw new Error("Refresh failed: No new token returned");
+        }
+
+        localStorage.setItem("token", newToken);
+        if (newRefreshToken) {
+          localStorage.setItem("refreshToken", newRefreshToken);
+        }
+
+        api.defaults.headers.common["Authorization"] = `Bearer ${newToken}`;
+        originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+
+        processQueue(null, newToken);
+        isRefreshing = false;
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        processQueue(refreshError, null);
+        isRefreshing = false;
+
+        console.error(
+          "Session expired completely. Logging out...",
+          refreshError,
+        );
+
+        store.dispatch(logOut());
+
+        return Promise.reject(refreshError);
+      }
     }
+
+    return Promise.reject(err);
+  },
 );
 
 export default api;
